90060e5102f6a2dcbbca3d05dbacfb469726d1a6
Lachlan <lachlanwaugh8@gmail.com>
2024-02-07 21:58:31 +1100

    Added rust lsp server. For some reason the package is rust-analyzer, but its called rust_analyzer (maybe because its being used as a var name?).

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -5,7 +5,7 @@ local capabilities = configs.capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "gopls", "pylsp" }
+local servers = { "html", "cssls", "tsserver", "clangd", "gopls", "pylsp", "rust_analyzer" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
